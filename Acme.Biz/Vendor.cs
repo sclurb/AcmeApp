2208440a2571cd50342b9621808acec10af00167
sclurb <sclurb@outlook.com>
2020-10-20 10:35:29 -0400

    Add project files.

---
+++ Acme.Biz/Vendor.cs
@@ -0,0 +1,115 @@
+﻿using Acme.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages the vendors from whom we purchase our inventory.
+    /// </summary>
+    public class Vendor 
+    {
+        public enum IncludeAddress { yes, no };
+        public enum SendCopy { yes, no };
+        public int VendorId { get; set; }
+        public string CompanyName { get; set; }
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Sends an email to welcome a new vendor.
+        /// </summary>
+        /// <returns></returns>
+        public string SendWelcomeEmail(string message)
+        {
+            var emailService = new EmailService();
+            var subject = ("Hello " + this.CompanyName).Trim();
+            var confirmation = emailService.SendMessage(subject,
+                                                        message, 
+                                                        this.Email);
+            return confirmation;
+        }
+
+
+
+        /// <summary>
+        /// Sends a product order to vendor.
+        /// </summary>
+        /// <param name="product">Product to Order</param>
+        /// <param name="quantity">Quantity of the product to order.</param>
+        /// <param name="deliverBy">Requested Delivery date.</param>
+        /// <param name="instructions">Delivery INstructions</param>
+        /// <returns></returns>
+        public OperationResult PlaceOrder(Product product, int quantity, DateTimeOffset? deliverBy = null, string instructions = "standard delivery")
+        {
+            if (product == null) throw new ArgumentNullException(nameof(product));
+            if (quantity <= 0) throw new ArgumentOutOfRangeException(nameof(quantity));
+            if (deliverBy <= DateTimeOffset.Now) throw new ArgumentOutOfRangeException(nameof(deliverBy));
+            var success = false;
+            var orderTextBuilder = new StringBuilder ("Order from Acme, Inc" + System.Environment.NewLine +
+                "Product: " + product.ProductCode + System.Environment.NewLine +
+                "Quantity: " + quantity);
+            if (deliverBy.HasValue)
+            {
+                orderTextBuilder.Append(System.Environment.NewLine +
+                    "Deliver By: " + deliverBy.Value.ToString("d"));
+            }
+            if (!String.IsNullOrWhiteSpace(instructions))
+            {
+                orderTextBuilder.Append(System.Environment.NewLine +
+                    "Instructions: " + instructions);
+            }
+            var orderText = orderTextBuilder.ToString();
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage("New Order", orderText, this.Email);
+            if (confirmation.StartsWith("Message sent:"))
+            {
+                success = true;
+            }
+            var operationResult = new OperationResult(success, orderText);
+            return operationResult;
+        }
+        /// <summary>
+        /// Sends a product order to vendor
+        /// </summary>
+        /// <param name="product">Product to order.</param>
+        /// <param name="quantity">Quantity of the product to order.</param>
+        /// <param name="includeAddress">True to include the shipping address</param>
+        /// <param name="SendCopy">True to send a copy of the email to the current</param>
+        /// <returns></returns>
+        public OperationResult PlaceOrder(Product product, int quantity, IncludeAddress includeAddress, SendCopy sendCopy)
+        {
+            var orderText = "Test";
+            if (includeAddress == IncludeAddress.yes) orderText += " With Address";
+
+            if (sendCopy == SendCopy.yes) orderText += " With Copy";
+            var operationResult = new OperationResult(true, orderText);
+            return operationResult;
+        }
+
+        public override string ToString()
+        {
+            string vendorInfo = "Vendor: " + this.CompanyName;
+            string result;
+
+            result = vendorInfo?.ToLower();
+            result = vendorInfo?.ToUpper();
+            result = vendorInfo?.Replace("Vendor", "Supplier");
+            var length = vendorInfo?.Length;
+            var index = vendorInfo?.IndexOf(":");
+            var begins = vendorInfo?.StartsWith("Vendor");
+            return vendorInfo;
+        }
+
+        public string PrepareDirections()
+        {
+            var directions = @"Insert \r\n to define a new line";
+            return directions;
+        }
+
+    }
+
+}
